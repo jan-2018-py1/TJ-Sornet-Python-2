062d21b72dc78c12881ac9200d75e00a0650ebc3
TJ Sornet <tsornet@gmail.com>
2018-02-24 13:39:27 -0800

    Courses django app - initial commit

---
+++ courses/apps/course/models.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models
+
+# Create your models here.
+class Course(models.Model):
+    name = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class Description(models.Model):
+    desc = models.TextField(null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    course = models.ForeignKey(Course, related_name="descriptions")
---
+++ courses/apps/course/views.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.shortcuts import render, HttpResponse, redirect
+from .models import *
+
+# Create your views here.
+def index(req):
+    context = {
+        "desc": Description.objects.all()  
+    }
+    return render(req, 'course/index.html', context)
+
+def add(req):
+    id = Course.objects.create(name=req.POST['course_name'])
+    Description.objects.create(desc=req.POST['course_desc'], course=id)
+    return redirect("/")
+
+def delete(req, id):
+    context = {
+        "data": Description.objects.get(id=id)
+    }
+    return render(req, 'course/remove.html', context)
+
+def destroy(req):
+    Course.objects.get(id=req.POST['id']).delete()
+    return redirect('/')
+
+
